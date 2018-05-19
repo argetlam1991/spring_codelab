40afc8eb391fb52ed2328a0e00d05561aa509c91
Gu Han <guhan@Gus-MacBook-Pro.local>
2018-05-18 22:28:50 -0700

    clean demo

--- demo/src/main/java/com/celesteeist/developer/demo/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.celesteeist.developer.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-interface CarRepository extends JpaRepository<Car, Long>{
-	
-}
