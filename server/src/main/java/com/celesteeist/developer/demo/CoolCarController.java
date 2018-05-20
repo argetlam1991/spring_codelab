a12ce37d4a420b20fb4992bb1779517f08f1400e
Gu Han <guhan@Gus-MacBook-Pro.local>
2018-05-19 17:31:27 -0700

    implement client

--- server/src/main/java/com/celesteeist/developer/demo/CoolCarController.java
+++ server/src/main/java/com/celesteeist/developer/demo/CoolCarController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 public class CoolCarController {
@@ -15,6 +16,7 @@ public class CoolCarController {
 	}
 	
 	@GetMapping("/cool-cars")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public Collection<Car> coolCars() {
 		repository.findAll();
 		return repository.findAll().stream()
